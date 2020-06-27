520065de6010effb8a75dee44f55c6f7b221a3bc
Maciej Kalita <maciej.kalita@gmail.com>
2020-06-27 23:24:43 +0200

    Add titleClickHandler add .active to clicked link

--- js/script.js
+++ js/script.js
@@ -8,17 +8,22 @@
 const titleClickHandler = function(event){
   console.log('Link was clicked!', event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
+    console.log('link .active removed', activeLink);
   }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  const clickedElement = this;
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+  console.log('clicked link .active added', clickedElement);
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts article.active');
 

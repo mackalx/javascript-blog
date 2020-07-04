4fef19227186f3fd12472be87e00ffbe0eb77f83
Maciej Kalita <maciej.kalita@gmail.com>
2020-07-04 16:56:10 +0200

    Add function calculateTagsParams to find min and max count of tags

--- js/script.js
+++ js/script.js
@@ -73,6 +73,20 @@ const generateTitleLinks = function(customSelector = ''){
 
 generateTitleLinks();
 
+const calculateTagsParams = function(tags) {
+  const params = {max: 0, min: 999999}; //
+  for(let tag in tags){
+    console.log(tag + ' is used ' + tags[tag] + ' times');
+    if(tags[tag] > params.max){
+      params.max = tags[tag];
+    }
+    if(tags[tag] < params.min){
+      params.min = tags[tag];
+    }
+  }
+  return params;
+};
+
 const generateTags = function(){
   /* [NEW] create a new variable allTags with an empty array (in this case changed to object) */
   let allTags = {};
@@ -121,6 +135,9 @@ const generateTags = function(){
   }
   /* [NEW] find list of tags in right column */
   const tagList = document.querySelector('.tags');
+
+  const tagsParams = calculateTagsParams(allTags);
+  console.log('tagsParams:', tagsParams)
 
   /* [NEW] create variable for all links HTML code */
   let allTagsHTML = '';
